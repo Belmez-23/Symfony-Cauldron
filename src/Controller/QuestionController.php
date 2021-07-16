2ddc46fa31f317fdf66b5524e3db9b013feb7e19
Username <afoniabrando@mail.ru>
2021-07-16 15:34:31 +0300

    chapter 4 problem: [Semantical Error] The annotation "@Route" in method App\Controller\QuestionController::homepage() was never imported. Did you maybe forget to add a "use" statement for this annotation? in /home/a1/PhpstormProjects/cauldron/config/routes/../../src/Controller/ (which is being imported from "/home/a1/PhpstormProjects/cauldron/config/routes/annotations.yaml"). Make sure to use PHP 8+ or that annotations are installed and enabled.

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -6,8 +6,22 @@ use Symfony\Component\HttpFoundation\Response;
 
 class QuestionController
 {
+    /**
+     * @Route("/")
+     */
     public function homepage()
     {
         return new Response('What a bewitching controller we have conjured!');
+    }
+
+    /**
+     * @Route("/questions/how-to-tie-my-shoes-with-magic")
+     */
+    public function show($slug)
+    {
+        return new Response(sprintf(
+            'Future page to show the question "%s"!',
+            ucwords(str_replace('-', ' ', $slug))
+        ));
     }
 }
