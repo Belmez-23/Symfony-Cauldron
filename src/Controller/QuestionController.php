03fcdd1fcd83dd8ac2d78c5704774517254ced8f
Username <afoniabrando@mail.ru>
2021-07-19 13:02:41 +0300

    p 2 ch 4

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -1,11 +1,14 @@
 <?php
+
 namespace App\Controller;
+
 use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Contracts\Cache\CacheInterface;
 use Twig\Environment;
+
 class QuestionController extends AbstractController
 {
     /**
@@ -16,10 +19,13 @@ class QuestionController extends AbstractController
         /*
         // fun example of using the Twig service directly!
         $html = $twigEnvironment->render('question/homepage.html.twig');
+
         return new Response($html);
         */
+
         return $this->render('question/homepage.html.twig');
     }
+
     /**
      * @Route("/questions/{slug}", name="app_question_show")
      */
@@ -31,9 +37,13 @@ class QuestionController extends AbstractController
             'Maybe... try saying the spell backwards?',
         ];
         $questionText = 'I\'ve been turned into a cat, any *thoughts* on how to turn back? While I\'m **adorable**, I don\'t really care for cat food.';
+
         $parsedQuestionText = $cache->get('markdown_'.md5($questionText), function() use ($questionText, $markdownParser) {
             return $markdownParser->transformMarkdown($questionText);
         });
+
+        //dd($markdownParser);
+
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
             'questionText' => $parsedQuestionText,
